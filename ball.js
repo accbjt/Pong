b0a7a814a559d98290dd5005eda37c3e46d640f3
Bill Tran <bjtran1234@gmail.com>
2014-02-10 18:57:02 -0800

    Added ball Logic

--- ball.js
+++ ball.js
@@ -4,19 +4,46 @@ function Ball() {
 	this.width = 20
 	this.height = 20
 
-	this.x = game.width/2 -this.width
-	this.y = game.height/2 -this.height
+	this.reset()
 
-	this.yVelocity = 10
 }
 
 Ball.prototype = Object.create(Entity.prototype)
 Ball.prototype.constructor = Ball
 
+Ball.prototype.reset = function(){
+	this.x = game.width/2 -this.width
+	this.y = game.height/2 -this.height
+
+	var min = -5, max = 5
+	this.yVelocity = Math.floor(Math.random() * (max - min + 1) + min)
+	this.xVelocity = Math.random() > 0.5 ? 5 : -5
+}
 Ball.prototype.update = function(){
 	Entity.prototype.update.apply(this, arguments) // super
 
 	if (this.y > game.height - this.height || this.y < 0){
 		this.yVelocity *= -1
+	}
+
+	if (this.x > game.width){
+		this.reset()
+	}
+
+	if (this.x < 0){
+		this.reset()
+	}
+
+	if (this.intersect(game.bot)){
+		var hitter = game.bot
+	} else if (this.intersect(game.player)){
+		var hitter = game.player
+	}
+
+	if (hitter){
+		this.xVelocity *= -1.1
+		this.yVelocity *= -1.1
+
+		this.yVelocity += hitter.yVelocity / 4
 	}
 }
